956a39ba0d84e4af8de7acd23714e92bffefee5f
Luke Rogers <luke@dmptr.com>
2020-01-04 10:51:38 +1300

    Namespaced URLs.
    Signed-off-by: Luke Rogers <luke@dmptr.com>

--- foxtail_contact/__init__.py
+++ foxtail_contact/__init__.py
@@ -1,4 +1,4 @@
 """ A simple contact form for Django """
-__version__ = '0.3.1'
+__version__ = '0.4.0'
 
 default_app_config = "foxtail_contact.apps.ContactConfig"
--- foxtail_contact/urls.py
+++ foxtail_contact/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'contact'
 urlpatterns = [
     path('', views.ContactView.as_view(), name='contact')
 ]
